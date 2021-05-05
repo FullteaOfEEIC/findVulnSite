b1cb6e886099f085709fb3f43de2387c54c826be
Rikuta Furuta <rikuta@furutan.com>
2021-05-05 21:44:58 +0900

    fix bugs

--- src/scantool.py
+++ src/scantool.py
@@ -6,7 +6,6 @@ from chardet.universaldetector import UniversalDetector
 from tqdm.auto import tqdm
 from parallel_requests.session import extended_session
 from multiprocessing import cpu_count
-import time
 
 def check_encoding(file_path):
     detector = UniversalDetector()
@@ -27,24 +26,26 @@ def dirb(domain, wordlist="/usr/share/dirb/wordlists/common.txt"):
     else:
         urls = ["http://"+domain, "https://"+domain]
     retval = copy(urls)
-    batch_size = 8
+    batch_size = 16
     while len(urls)>0:
         candidate = urls.pop(0)
         responses = []
         method_args = [{"url":candidate+"/"+word, "method":"get"} for word in words]
         for batch in tqdm(range(0, len(method_args), batch_size)):
-            with extended_session() as session:
-                _responses = session.parallel_request(
-                    method_args=method_args[batch:batch+batch_size],
-                    max_workers=cpu_count()*5
-                )
-            responses += _responses
+            try:
+                with extended_session() as session:
+                    _responses = session.parallel_request(
+                        method_args=method_args[batch:batch+batch_size],
+                        max_workers=cpu_count()*5
+                    )
+                responses += _responses
+            except:
+                continue
         for response, method in zip(responses, method_args):
             if response.status_code!=404:
                 print(method["url"], response.status_code)
                 retval.append(method["url"])
                 urls.append(method["url"])
-        time.sleep(3)
 
     return retval
 
