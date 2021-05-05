3049a9a67dc1026f21b504695bba9420907c2cfb
Rikuta Furuta <rikuta@furutan.com>
2021-05-05 23:11:52 +0900

    fix bugs

--- src/scantool.py
+++ src/scantool.py
@@ -7,7 +7,7 @@ import uuid
 from tqdm.auto import tqdm
 from parallel_requests.session import extended_session
 from multiprocessing import cpu_count
-
+import os
 
 def dirb(domain, wordlist="/usr/share/dirb/wordlists/big.txt"):
     if re.match("https?://",domain):
@@ -15,15 +15,16 @@ def dirb(domain, wordlist="/usr/share/dirb/wordlists/big.txt"):
     else:
         domains = ["http://"+domain,"https://"+domain]
     retval = []
-    dirb_retval = re.compile("^\+ (https?://.+) (CODE:\d+|SIZE:\d+)$")
+    dirb_retval = re.compile("^\+ (https?:\/\/.+) \(CODE:\d+\|SIZE:\d+\)$")
     for domain in domains:
         random_name = str(uuid.uuid4())
         subprocess.run(["dirb", domain, "-o", random_name,"-w"])
         with open(random_name,"r") as fp:
             for line in fp:
-                match = re.match(line.strip())
+                match = dirb_retval.match(line.strip())
                 if match:
                     retval.append(match.group(1))
+        os.remove(random_name)
 
 
     return retval
