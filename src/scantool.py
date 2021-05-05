e35d722e6a84d79e8e688688f784f4cd08722d8d
Rikuta Furuta <rikuta@furutan.com>
2021-05-05 21:51:45 +0900

    fix bug

--- src/scantool.py
+++ src/scantool.py
@@ -24,7 +24,7 @@ def dirb(domain, wordlist="/usr/share/dirb/wordlists/common.txt"):
     if re.match("https?://",domain):
         urls = [domain]
     else:
-        urls = ["http://"+domain, "https://"+domain]
+        urls = ["http://"+domain]
     retval = copy(urls)
     batch_size = 16
     while len(urls)>0:
@@ -32,20 +32,18 @@ def dirb(domain, wordlist="/usr/share/dirb/wordlists/common.txt"):
         responses = []
         method_args = [{"url":candidate+"/"+word, "method":"get"} for word in words]
         for batch in tqdm(range(0, len(method_args), batch_size)):
-            try:
-                with extended_session() as session:
-                    _responses = session.parallel_request(
-                        method_args=method_args[batch:batch+batch_size],
-                        max_workers=cpu_count()*5
-                    )
-                responses += _responses
-            except:
-                continue
+            with extended_session() as session:
+                _responses = session.parallel_request(
+                    method_args=method_args[batch:batch+batch_size],
+                    max_workers=cpu_count()*5
+                )
+            responses += _responses
         for response, method in zip(responses, method_args):
             if response.status_code!=404:
                 print(method["url"], response.status_code)
                 retval.append(method["url"])
-                urls.append(method["url"])
+                if response.status_code==200:
+                    urls.append(method["url"])
 
     return retval
 
